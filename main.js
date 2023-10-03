633abd000da04e5150fc32d9ddb93a77d96f4d89
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-10-03 21:54:56 +0530

    testing some text

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ function toggleMobileMenu() {
   let hum = document.getElementById("hamburger-icon");
   hum.classList.toggle("open");
 }
+
+// let res = document.getElementsByClassName("footer__copy")[0];
+// console.log(new Date().getFullYear(), res.innerHTML);
